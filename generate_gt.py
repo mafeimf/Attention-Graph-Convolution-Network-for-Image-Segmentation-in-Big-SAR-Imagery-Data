a7ee2f8043920ce2e8875e4e744d1c101d3effbb
fei ma <33041192+mafeimf@users.noreply.github.com>
2020-03-28 16:25:03 +0800

    Update generate_gt.py

--- generate_gt.py
+++ generate_gt.py
@@ -46,27 +46,7 @@ def generate_gt_fangchenggang():
     out_GT[(gt_original[:,:,0]<=50) *( gt_original[:,:,1]<=50)*(gt_original[:,:,2]>=200)]=2
     out_GT[(gt_original[:,:,0]>=200) *( gt_original[:,:,1]>=200)*(gt_original[:,:,2]>=200)]=4
     np.save('gt_numerical_fangchenggang.npy',out_GT)
-def generate_gt_zhanjiang():
-    gt_original=image.imread('../../datasets/zhanjiang/ground-truth3.jpg')
-     #gt_original=image.imread('../../datasets/fangchenggang/ground_truth2.jpg')
-    out_GT=np.zeros((gt_original.shape[0],gt_original.shape[1]),dtype="uint8")+3
-    out_GT[(gt_original[:,:,0]<=50) *( gt_original[:,:,1]<=50)*(gt_original[:,:,2]<=50)]=0
-    out_GT[(gt_original[:,:,0]>=200) *( gt_original[:,:,1]<=50)*(gt_original[:,:,2]<=50)]=1
-    out_GT[(gt_original[:,:,0]>=200) *( gt_original[:,:,1]>=200)*(gt_original[:,:,2]<=50)]=2
-    out_GT[(gt_original[:,:,0]<=50) *( gt_original[:,:,1]<=50)*(gt_original[:,:,2]>=200)]=1
-    out_GT[(gt_original[:,:,0]>=200) *( gt_original[:,:,1]>=200)*(gt_original[:,:,2]>=200)]=3
-    np.save('gt_numerical_zhanjiang.npy',out_GT)
-    #np.save('gt_numerical_fangchenggang.npy',out_GT)
-def generate_gt_shanxiweinan():
-    gt_original=image.imread('../../datasets/ShanxiWeinan/陕西渭南GT_裁剪.jpg')
-     #gt_original=image.imread('../../datasets/fangchenggang/ground_truth2.jpg')
-    out_GT=np.zeros((gt_original.shape[0],gt_original.shape[1]),dtype="uint8")+1
-    out_GT[(gt_original[:,:,0]>=200) *( gt_original[:,:,1]>=200)*(gt_original[:,:,2]<=50)]=1
-    out_GT[(gt_original[:,:,0]>=200) *( gt_original[:,:,1]<=50)*(gt_original[:,:,2]<=50)]=0
-    out_GT[(gt_original[:,:,0]<=50) *( gt_original[:,:,1]<=50)*(gt_original[:,:,2]>=200)]=2
-    
-    np.save('gt_numerical_shanxiweinan.npy',out_GT)
-    #np.save('gt_numerical_fangchenggang.npy',out_GT)
+
 def generate_gt_shanxipucheng():
     gt_original=image.imread('../../datasets/陕西蒲城/陕西蒲城GT_裁剪.jpg')
      #gt_original=image.imread('../../datasets/fangchenggang/ground_truth2.jpg')
